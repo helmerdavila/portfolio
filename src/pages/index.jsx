79b61c36cbd0e61e0ac3e888c0b1aa915f9b17d7
Helmer <helmer290192@hotmail.com>
2019-01-19 20:09:36 -0500

    What i do section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,8 +4,8 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Presentation from '../components/presentation'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faJsSquare, faPhp, faCss3Alt, faAws } from '@fortawesome/free-brands-svg-icons'
-import { faGem } from '@fortawesome/free-solid-svg-icons';
+import { faJsSquare, faPhp, faCss3Alt, faAws, faChrome } from '@fortawesome/free-brands-svg-icons'
+import { faGem, faCode, faServer } from '@fortawesome/free-solid-svg-icons';
 import styles from './index.module.scss'
 import classnames from "classnames"
 
@@ -23,6 +23,46 @@ const IndexPage = () => (
           (Laravel/Zend/Codeigniter), Ruby (Rails) and frontend code in Javascript
           (Vue/Angular/React), HTML, CSS (SCSS/Stylus).
         </p>
+      </div>
+    </section>
+    <section className={classnames(styles.what_i_do, 'section')}>
+      <div className="container">
+        <h1 className={classnames(styles.title, 'title is-1 has-text-centered')}>What I do</h1>
+        <div className="columns">
+          <div className="column">
+            <div className={styles.service}>
+              <span className={styles.the_icon}>
+                <FontAwesomeIcon icon={faChrome} size="3x"/>
+              </span>
+              <div className="content">
+                <h3>Frontend Development</h3>
+                <p>I code for the browser using languages like HTML(5), CSS(3) and JavaScript. Building sites from scratch, with or without frameworks. I've worked with hibrid mobile apps too.</p>
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className={styles.service}>
+              <span className={styles.the_icon}>
+                <FontAwesomeIcon icon={faCode} size="3x"/>
+              </span>
+              <div className="content">
+                <h3>Backend Development</h3>
+                <p>Code the requirements and restrictions for the business, using backend frameworks with good scalability option and quick development of new features.</p>
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className={styles.service}>
+              <span className={styles.the_icon}>
+                <FontAwesomeIcon icon={faServer} size="3x"/>
+              </span>
+              <div className="content">
+                <h3>Server managment</h3>
+                <p>Upload new changes in apps with tools for automated deployments. Setting testing and production environments, avoiding security mistakes.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
     <section className="section">
