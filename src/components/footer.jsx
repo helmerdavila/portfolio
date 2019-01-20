503b2b893bfb2e852f23459b1309228c2160416b
Helmer <helmer290192@hotmail.com>
2019-01-19 20:33:43 -0500

    Added footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
+import classnames from 'classnames'
+import styles from './footer.module.scss'
+
+const Footer = () => (
+  <footer className={classnames(styles.footer, 'footer')}>
+    <div className="container">
+      <div className="content has-text-centered">
+        <a className={styles.rounded_link} href="https://www.linkedin.com/in/helmerdavila/" target="_blank">
+          <FontAwesomeIcon icon={faLinkedinIn} fixedWidth/>
+        </a>
+        <a className={styles.rounded_link} href="https://www.github.com/helmerdavila" target="_blank">
+          <FontAwesomeIcon icon={faGithub} fixedWidth/>
+        </a>
+        <p>
+          <span>© {new Date().getFullYear()}</span> // <span>Helmer Dávila</span> // <span>Powered by Gatsby.</span>
+        </p>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer
