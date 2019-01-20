289f91815a9dc494d7f9e38e76ff8ea10c310b56
Helmer <helmer290192@hotmail.com>
2019-01-20 13:46:42 -0500

    Moved bulma to global styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
-import './layout.scss'
 import Footer from './footer';
 
 const Layout = ({ children }) => (
