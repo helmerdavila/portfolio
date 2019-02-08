f73f76bc4785a17cd77ff27aa0430a4640ee8cd2
Helmer <helmer290192@hotmail.com>
2019-02-07 20:59:04 -0500

    Added code samples area

---
+++ src/components/Images/BattleshipImage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+const battleshipImage = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          placeholderImage: file(
+            relativePath: { eq: "battleship_mockup.png" }
+          ) {
+            childImageSharp {
+              fluid(maxWidth: 300) {
+                ...GatsbyImageSharpFluid_tracedSVG
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+      )}
+    />
+  )
+}
+export default battleshipImage
