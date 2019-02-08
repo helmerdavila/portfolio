0106cd288c69d7cab60cb04eb1ee7cf9a22b7e63
Helmer <helmer290192@hotmail.com>
2019-02-07 20:03:57 -0500

    Added Kloe and Vicuna Beer image

---
+++ src/components/Images/VicunaImage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+const vicunaImage = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "vicunia_beer_mockup.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      }
+    `}
+      render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+    />
+  )
+}
+export default vicunaImage
