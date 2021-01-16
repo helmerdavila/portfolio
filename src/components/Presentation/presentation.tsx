452a8883b5c43b7995fcafee1c0f93c2ec3f35ae
Helmer <helmer290192@hotmail.com>
2021-01-15 19:34:58 -0500

    Presentation translated

--- src/components/Presentation/presentation.tsx
+++ src/components/Presentation/presentation.tsx
@@ -3,9 +3,11 @@ import Header from '../Layouts/header';
 import Typing from 'react-typing-animation';
 import classNames from 'classnames';
 import { ProfileContext } from '../../pages';
+import { useTranslation } from 'gatsby-plugin-react-i18next';
 
-const Presentation = () => {
+const Presentation = (): JSX.Element => {
   const context = useContext(ProfileContext);
+  const { t } = useTranslation();
 
   return (
     <div className="min-h-screen w-full first-section bg-black">
@@ -13,19 +15,19 @@ const Presentation = () => {
       <div id="who_am_i" className="min-h-screen flex justify-center items-center">
         <div className={classNames({ overlay: context.isLightTheme, 'overlay-night': !context.isLightTheme })} />
         <div className="text-center z-10">
-          <h3 className="text-3xl font-bold text-white">Hi, my name is</h3>
+          <h3 className="text-3xl font-bold text-white">{t('hi_my_name_is')}</h3>
           <h1 className="text-6xl font-bold text-white">Helmer Dávila</h1>
-          <h3 className="text-3xl font-bold text-white">Software Engineer and Fullstack Developer</h3>
+          <h3 className="text-3xl font-bold text-white">{t('software_engineer')}</h3>
           <h2 className="text-5xl font-bold inline-block text-white">
-            <span>and I build </span>
+            <span>{t('and_i_build')}</span>
             <Typing loop speed={50}>
-              <span>e-commerce websites</span>
+              <span>{t('ecommerce_websites')}</span>
               <Typing.Reset count={1} delay={1000} />
-              <span>landing pages</span>
+              <span>{t('landing_pages')}</span>
               <Typing.Reset count={1} delay={2000} />
-              <span>web applications</span>
+              <span>{t('web_applications')}</span>
               <Typing.Reset count={1} delay={300} />
-              <span>mobile applications</span>
+              <span>{t('mobile_applications')}</span>
               <Typing.Reset delay={4000} />
             </Typing>
           </h2>
