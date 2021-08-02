9b621fafa62bb7d0bbd8927a4d759ec7b1180986
Helmer <11061182+helmerdavila@users.noreply.github.com>
2021-08-01 20:54:09 -0400

    Updating version of Node to LTS

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,14 +1,12 @@
 import React, { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// @ts-ignore
 import { withScroll } from 'react-window-decorators';
 import classNames from 'classnames';
 import { animateScroll as scroll, Link as LinkScroll } from 'react-scroll';
 import { ProfileContext } from '../../pages';
 import { faDownload, faHSquare } from '@helmerdavila/fontawesomehelmer/pro-duotone-svg-icons';
-import { Link, useI18next, useTranslation } from 'gatsby-plugin-react-i18next';
-// @ts-ignore
 import DarkModeToggle from 'react-dark-mode-toggle';
+import useTranslations from '../UseTranslations';
 
 const Header = (props: { scrollPositionY: number }) => {
   const context = useContext(ProfileContext);
@@ -19,12 +17,7 @@ const Header = (props: { scrollPositionY: number }) => {
       'bg-gray-800': props.scrollPositionY > 0 && !context.isLightTheme,
     },
   );
-  const navbarLinkClass = classNames('mx-3 cursor-pointer transition duration-200 ease-in-out hidden md:block', {
-    'text-white': props.scrollPositionY === 0 || (props.scrollPositionY > 0 && !context.isLightTheme),
-    'text-dark': props.scrollPositionY > 0 && context.isLightTheme,
-  });
-  const { languages, originalPath } = useI18next();
-  const { t } = useTranslation();
+  const { cv_link, download_cv } = useTranslations();
 
   return (
     <nav className={navbarClass}>
@@ -38,42 +31,27 @@ const Header = (props: { scrollPositionY: number }) => {
         >
           <FontAwesomeIcon icon={faHSquare} size="2x" />
         </div>
-        <LinkScroll to="who_am_i" spy={true} className={navbarLinkClass} smooth>
-          {t('who_am_i')}
-        </LinkScroll>
-        <LinkScroll to="about_me" spy={true} className={navbarLinkClass} smooth>
-          {t('about_me')}
-        </LinkScroll>
-        <LinkScroll to="what_i_do" spy={true} className={navbarLinkClass} smooth>
-          {t('what_i_do')}
-        </LinkScroll>
-        <LinkScroll to="my_stack" spy={true} className={navbarLinkClass} smooth>
-          {t('my_stack')}
-        </LinkScroll>
-        <LinkScroll to="projects" spy={true} className={navbarLinkClass} smooth>
-          {t('projects')}
-        </LinkScroll>
       </div>
       <div className="flex flex-row items-center">
-        <ul className="flex flex-row justify-between">
-          {languages.map((lng) => (
-            <li key={lng}>
-              <Link
-                to={originalPath}
-                className={classNames('mr-3 uppercase', {
-                  'text-white': props.scrollPositionY === 0 || !context.isLightTheme,
-                  'text-black': props.scrollPositionY > 0 && context.isLightTheme,
-                })}
-                language={lng}
-              >
-                {lng}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        {/*<ul className="flex flex-row justify-between">*/}
+        {/*  {languages.map((lng) => (*/}
+        {/*    <li key={lng}>*/}
+        {/*      <Link*/}
+        {/*        to={originalPath}*/}
+        {/*        className={classNames('mr-3 uppercase', {*/}
+        {/*          'text-white': props.scrollPositionY === 0 || !context.isLightTheme,*/}
+        {/*          'text-black': props.scrollPositionY > 0 && context.isLightTheme,*/}
+        {/*        })}*/}
+        {/*        language={lng}*/}
+        {/*      >*/}
+        {/*        {lng}*/}
+        {/*      </Link>*/}
+        {/*    </li>*/}
+        {/*  ))}*/}
+        {/*</ul>*/}
         <DarkModeToggle size={50} onChange={context.toggleTheme} checked={!context.isLightTheme} />
         <a
-          href={t('cv_link')}
+          href={cv_link}
           target="_blank"
           rel="noreferrer"
           className={classNames(
@@ -86,7 +64,7 @@ const Header = (props: { scrollPositionY: number }) => {
           )}
         >
           <FontAwesomeIcon icon={faDownload} />
-          <span className="ml-2">{t('download_cv')}</span>
+          <span className="ml-2">{download_cv}</span>
         </a>
       </div>
     </nav>
