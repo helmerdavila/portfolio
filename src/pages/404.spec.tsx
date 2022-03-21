bdee5847a4b2d62ab5352a6958c49152cd478071
Helmer <11061182+helmerdavila@users.noreply.github.com>
2022-03-21 01:41:13 -0400

    First version of tests

---
+++ src/pages/404.spec.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFoundPage from './404';
+import { useStaticQuery } from 'gatsby';
+
+beforeEach(() => {
+  (useStaticQuery as jest.Mock).mockImplementationOnce(() => ({
+    site: {
+      siteMetadata: {
+        title: `Default Starter`,
+      },
+    },
+  }));
+});
+
+it('renders without issues', () => {
+  const { getByText } = render(<NotFoundPage />);
+
+  expect(getByText('NOT FOUND')).toBeInTheDocument();
+});
