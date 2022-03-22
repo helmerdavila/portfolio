41ad82ac87ecd4619d6e192218e1e428c5b6042a
helmer <11061182+helmerdavila@users.noreply.github.com>
2022-03-22 01:08:34 -0400

    First try of create the translations

---
+++ src/components/AboutMe/AboutMe.spec.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import { useStaticQuery } from 'gatsby';
+import React from 'react';
+import { Layout } from '../Layout';
+import AboutMe from './AboutMe';
+import en from '../../../config/translations/en.json';
+
+beforeEach(() => {
+  (useStaticQuery as jest.Mock).mockResolvedValue(() => ({
+    rawData: { edges: [{ item: { node: { name: 'sample', translations: en } } }] },
+  }));
+});
+
+it('renders without problems', () => {
+  render(
+    <Layout pageContext={{ locale: 'en' }}>
+      <AboutMe />
+    </Layout>,
+  );
+
+  expect(true).toBe(true);
+});
