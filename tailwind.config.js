084a7408ab67b545a5848f456de4cb8b32dcdc02
Helmer <11061182+helmerdavila@users.noreply.github.com>
2022-12-23 23:56:08 -0500

    Upgraded Tailwind and adding font as class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/components/**/*.{js,jsx,ts,tsx}', './src/pages/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        quicksand: ['Quicksand', 'sans-serif'],
+      },
       spacing: {
         '1/2': '50%',
         '1/3': '33.333333%',
