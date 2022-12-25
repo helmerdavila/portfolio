3645cb159945bc6ecd0dbb60455b754e9632771e
Helmer <11061182+helmerdavila@users.noreply.github.com>
2022-12-25 18:38:32 -0500

    Another traced SVG removal

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -59,7 +59,6 @@ const config: GatsbyConfig = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              tracedSVG: true,
               maxWidth: 800,
             },
           },
