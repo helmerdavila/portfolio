914c2128c657ec1bfcaaebc1270ca293c22daf0a
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-02-10 22:45:05 -0500

    Removing Jest references

--- jest-preprocess.js
+++
@@ -1,5 +0,0 @@
-const babelOptions = {
-  presets: ['babel-preset-gatsby', '@babel/preset-typescript'],
-  plugins: ['@babel/plugin-transform-modules-commonjs'],
-};
-module.exports = require('babel-jest').default.createTransformer(babelOptions);
